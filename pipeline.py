5f7b185349076f276f083cfb3c7abbb6e7131e9a
Anwar Knyane <21956329+anvarknian@users.noreply.github.com>
2024-10-01 06:08:37 -0300

    - code refactored - fixed docker-compose.yml

--- pipeline.py
+++ pipeline.py
@@ -5,16 +5,15 @@ from dlt.sources.helpers.rest_client import RESTClient
 from dlt.sources.helpers.rest_client.paginators import JSONResponseCursorPaginator
 
 logger = logging.getLogger(__name__)
-FILE_URL = 'https://clinicaltrials.gov/api/v2'
+API_URL = 'https://clinicaltrials.gov/api/v2'
 
 client = RESTClient(
-    base_url=FILE_URL,
+    base_url=API_URL,
     paginator=JSONResponseCursorPaginator(cursor_param="pageToken", cursor_path="nextPageToken")
 )
 
-
-@dlt.resource(write_disposition='replace')
-def get_data():
+@dlt.resource(name='clinical_trials', max_table_nesting=2, write_disposition='replace')
+def clinical_trials_data():
     for page in client.paginate("/studies"):
         yield page
 
@@ -27,7 +26,7 @@ def load_clinical_trials() -> None:
     )
 
     # limit number of yield for dev purpose
-    load_info = pipeline.run(get_data().add_limit(10))
+    load_info = pipeline.run(clinical_trials_data().add_limit(10))
     logger.info(load_info)
 
 if __name__ == "__main__":
